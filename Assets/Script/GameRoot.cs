27f3711b0fbd28795844adc0e818f955fecd5331
Shinelightyiyi <14791651366@136.com>
2024-01-25 10:20:21 +0800

    游戏框架

---
+++ Assets/Script/GameRoot.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameRoot : MonoBehaviour
+{
+    private static GameRoot instance;
+    public static GameRoot Instance { get=>instance; }
+
+    public UIManager rootUIManager;
+
+    public void Awake()
+    {
+        rootUIManager = new UIManager();
+
+        if(instance == null )
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        GameObject go = GameObject.FindGameObjectWithTag("NormalCanvas");
+        DontDestroyOnLoad(go);
+    }
+
+
+
+
+}
